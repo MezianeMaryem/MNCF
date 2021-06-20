4d2266825f289ed6bc2bed9b6e71b38638c9b24c
Maryem MEZIANE <maryame.meziane@gmail.com>
2021-06-20 01:05:23 +0100

    waaa 3iiiiiiiiiiiiiiit

---
+++ app/src/main/java/com/example/mncf/DatabaseOpenHelper.java
@@ -0,0 +1,4 @@
+package com.example.mncf;
+
+public class DatabaseOpenHelper {
+}
---
+++ app/src/main/java/com/example/mncf/line20.java
@@ -0,0 +1,14 @@
+package com.example.mncf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class line20 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line20);
+    }
+}
