1d2a3879de7f5b5a299cc765e6515a84eb48ada2
Maryem MEZIANE <maryame.meziane@gmail.com>
2021-06-22 17:24:12 +0100

    grrrr

--- app/src/main/java/com/example/mncf/taxi.java
+++ app/src/main/java/com/example/mncf/taxi.java
@@ -1,25 +1,71 @@
 package com.example.mncf;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class taxi extends AppCompatActivity {
+EditText rname,rnumber,rarrive,rdate,rdepart,rheur;
 TextView commande;
+DatabaseReference firebaseDatabase;
+DatabaseReference databaseReference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_taxi);
+        rname=findViewById(R.id.name);
+        rnumber=findViewById(R.id.num);
+        rarrive=findViewById(R.id.arrivée);
+        rdepart=findViewById(R.id.depart);
+        rheur=findViewById(R.id.heure);
+        rdate=findViewById(R.id.date);
         commande=findViewById(R.id.btnCommande);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("user");
+
+
         commande.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent1 = new Intent(taxi.this,taxi2.class);
-                startActivity(intent1);
+                String name=rname.getText().toString();
+                String num=rnumber.getText().toString();
+                String dep=rdepart.getText().toString();
+                String arrive=rarrive.getText().toString();
+                String date=rdate.getText().toString();
+                String heure=rheur.getText().toString();
+
+                HashMap<String,String> userMap=new HashMap<>();
+                userMap.put("name",name);
+                userMap.put("num",num);
+                userMap.put("depart",dep);
+                userMap.put("arrivé",arrive);
+                userMap.put("dated",date);
+                userMap.put("heure",heure);
+
+                myRef.setValue(userMap);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/mncf/taxi2.java
+++ app/src/main/java/com/example/mncf/taxi2.java
@@ -1,14 +1,57 @@
 package com.example.mncf;
 
-import androidx.appcompat.app.AppCompatActivity;
+public class taxi2{
+    String name,num,arrive,depart,date,heure;
+    public taxi2() {}
+        public taxi2(String name,String num,String arrive,String depart, String date,String heure){
+                this.arrive = arrive;
+                this.name=name;
+                this.date=date;
+                this.depart=depart;
+                this.heure=heure;
+                this.num=num;
+        }
 
-import android.os.Bundle;
+        public String getName(){
+        return name;
+        }
+        public void setName(){
+        this.name=name;
+        }
 
-public class taxi2 extends AppCompatActivity {
+        public String getNum(){
+        return num;
+        }
+    public void setNum(){
+        this.num=num;
+    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_taxi2);
+        public String getDate(){
+        return date;
+         }
+    public void setDate(){
+        this.date=date;
+    }
+
+    public String getHeure(){
+        return heure;
+    }
+    public void setHeure(){
+        this.heure=heure;
+    }
+
+    public String getDepart(){
+        return depart;
+    }
+    public void setDepart(){
+        this.depart=depart;
+    }
+
+    public String getArrive(){
+        return arrive;
+    }
+    public void setArrive(){
+        this.arrive=arrive;
     }
 }
+
